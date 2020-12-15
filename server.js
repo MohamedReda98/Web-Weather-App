9635a09d0486c49db410d455137dfa43b11bbbe0
Mohamed Reda <alprofmohamed123@gmail.com>
2020-12-15 16:32:48 +0200

    this is is my first step is initializ my server in local host 3000

--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 projectData = {};
 
 // Require Express to run server and routes
+/* Express to run server and routes */
+const express = require('express');
 
-// Start up an instance of app
+/* Start up an instance of app */
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +17,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000,
+/* Spin up the server*/
+    server = app.listen(port, listening);
+function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`)
+}
+server;
