d71f42b6d771c28d864eae0249b7a2613d7df761
Mohamed Reda <alprofmohamed123@gmail.com>
2020-12-15 20:16:54 +0200

    second step is to make a async function which fetch the data from the site https://api.openweathermap.org then show the data as matric type

--- website/app.js
+++ website/app.js
@@ -3,3 +3,29 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+// get data
+const fetch = require('node-fetch');
+let zip = 90001;
+let newData = {};
+const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=f6aa84e5214941d9aab6e36585c2dc5d&units=metric`;
+const getApi =async () =>{
+    const response = await fetch(url,{
+        method:'POST',
+        credentials: "same-origin",
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify(newData),
+    });
+    try{
+        newData = await response.json();
+        console.log(newData.main.temp);
+        return newData;
+    }
+    catch (error){
+        console.log('error',error);
+    }
+}
+getApi();
