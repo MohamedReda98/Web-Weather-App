1cfb4db99d51b7a256258492dcf7e990bdb209d5
Mohamed Reda <alprofmohamed123@gmail.com>
2020-12-15 21:54:29 +0200

    get the zip code and felling inputs and check values then store in variables so we can use it later

--- website/app.js
+++ website/app.js
@@ -2,15 +2,26 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+function performAction() {
+    const inpZip = document.getElementById('zip').value;
+    const inpFeelings = document.getElementById('feelings').value;
+    if (inpZip=='' || typeof inpZip !='number' ){alert("Please enter valid zip code")}
+    else{
+        zip=inpZip;
+        console.log(getApi());
+    }
+}
 // Personal API Key for OpenWeatherMap API
 // get data
-const fetch = require('node-fetch');
-let zip = 90001;
+let zip = 85005;
 let newData = {};
 const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=f6aa84e5214941d9aab6e36585c2dc5d&units=metric`;
 const getApi =async () =>{
+    const fetch = require('node-fetch');
     const response = await fetch(url,{
         method:'POST',
         credentials: "same-origin",
@@ -28,4 +39,3 @@ const getApi =async () =>{
         console.log('error',error);
     }
 }
-getApi();
